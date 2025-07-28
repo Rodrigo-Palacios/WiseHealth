9e7d037f8994c0eede9f372ba4df638d23604ed4
Rodrigo-Palacios <rodrigo.palaciosc@outlook.com>
2025-07-28 14:12:53 -0600

    feat: Add form validation and persist data to local storage

---
+++ src/main.js
@@ -0,0 +1,44 @@
+import { showPeople } from "./showList";
+// seleciona el boton del form
+const btnForm = document.getElementById('buttonForm');
+
+// Crea un objeto donde que se almacena en listedPeople
+class Person {
+    constructor(name, phone, email, age, gender) {
+        this.name = name    
+        this.phone = phone
+        this.email = email
+        this.age = age
+        this.gender = gender    
+    }
+}
+
+// Almacena los objetos de la clase Person
+let listedPeople = [];
+
+btnForm.addEventListener('click', (e) => {
+    e.preventDefault();
+    
+    //Obtine los compos del form
+    let form = document.forms['newPerson'];
+    let name = form.name.value;
+    let phone = form.phone.value;
+    let email = form.email.value;
+    let age = form.age.value;
+    let gender = form.gender.value;
+
+    const validity = form.reportValidity();
+
+    if (validity) {
+        //Agrega los datos del form al array listedPeople
+         listedPeople.push(new Person(name, phone, email, age, gender));
+    
+        //Almacena listedPeople en localStorage
+        localStorage.setItem("list",JSON.stringify(listedPeople));
+
+        form.reset()
+    }
+        
+    showPeople();
+
+}) 
---
+++ src/showList.js
@@ -0,0 +1,8 @@
+export function showPeople() {
+  
+  const listPeople = localStorage.getItem("list");
+
+  console.log(listPeople);
+
+
+}
